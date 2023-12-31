e877c818b4acf849ce7288ec3c6444ab7463446c
Nicolas Lachise <contact@webdevlyon.fr>
2023-12-31 19:39:34 +0100

    Init project

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <Html class="bg-emerald-500 trello">
+  <div>
+    <NuxtPage />
+  </div></Html>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useBoardStore } from '../stores/boardStore'
+
+const boardStore = useBoardStore()
+
+const newColumnName = ref('')
+
+function addColumn() {
+  boardStore.addColumn(newColumnName.value)
+  newColumnName.value = ''
+}
+</script>
+
+<template>
+  <div class="board-wrapper">
+    <main class="board">
+      <BoardColumn
+        v-for="(column, columnIndex) in boardStore.board.columns"
+        :key="column.id"
+        :column="column"
+        :columnIndex="columnIndex"
+      />
+      <UContainer class="column">
+        <UInput
+          v-model="newColumnName"
+          type="text"
+          placeholder="Create new column"
+          icon="i-heroicons-plus-circle-solid"
+          @keyup.enter="addColumn"
+        />
+      </UContainer>
+    </main>
+  </div>
+</template>
