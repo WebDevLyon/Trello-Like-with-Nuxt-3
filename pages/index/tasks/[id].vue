be6bd7bb4bf8359f4d497bfd041b4290ee9f8bca
Nicolas Lachise <contact@webdevlyon.fr>
2024-01-01 12:04:03 +0100

    ouverture tache dans boite modal correction

---
+++ pages/index/tasks/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue';
+import { useBoardStore } from '~/stores/boardStore'
+
+const route = useRoute()
+const boardStore = useBoardStore()
+
+const task = computed(() => {
+  return boardStore.getTask(route.params.id)
+})
+</script>
+
+<template>
+  <div class="task-wrapper">
+    <div class="task-view">
+      <h1>{{ task.name }}</h1>
+      <p>{{ task.description }}</p>
+    </div>
+  </div>
+</template>
